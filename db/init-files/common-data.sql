2ac14a08e1a949863c00ab6d3a3e7d9cd5fb5a08
Gonçalo Durão <goncalo.durao@gmail.com>
2019-01-27 21:55:20 +0000

    Adds changes to allow DB connections.

--- db/init-files/common-data.sql
+++ db/init-files/common-data.sql
@@ -1,3 +1,3 @@
 INSERT INTO admin (id_admin,email,password,firstname,lastname,picture,status,create_date,modify_date) VALUES 
-(1,'admin@apisf.com','48f074c5a644e37973aa85c027ca055a7fe5c3f08f5cbb79874e981d078af45d$2a$08$A9bgZWiLwA8dXIztAtJyqg$','Developer','Admin',NULL,1,'2017-05-19 17:00:00.000',NULL)
+(1,'admin@apisf.com','48f074c5a644e37973aa85c027ca055a7fe5c3f08f5cbb79874e981d078af45d$2a$08$A9bgZWiLwA8dXIztAtJyqg$','Dev','Admin',NULL,1,'2017-05-19 17:00:00.000',NULL)
 
